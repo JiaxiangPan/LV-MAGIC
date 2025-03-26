0f558f4b79d1f46131c92f226fa69bf6c31221d3
panjiaxiang7 <panjiaxiang7@163.com>
2025-03-26 10:44:40 +0800

    update netlist_to_verilog.hpp

--- src/core/netlist_to_verilog.hpp
+++ src/core/netlist_to_verilog.hpp
@@ -7,9 +7,12 @@ void write_verilog(std::ifstream &ifs, std::ofstream &ofs)
     const std::string flag_inputs = "input";
     const std::string flag_outputs = "output";
     const std::string flag_wires = "wire";
-    const std::string  flag_end = ";";
+    const std::string flag_end = ";";
     const std::string flag_node1 = "inv1";
     const std::string flag_node2 = "nor2";
+    const std::string flag_node3 = "zero";
+    const std::string flag_node4 = "buf";
+    const std::string flag_node5 = "one";
 
     std::string input_statement;
     std::string output_statement;
@@ -18,8 +21,7 @@ void write_verilog(std::ifstream &ifs, std::ofstream &ofs)
     std::vector<std::string> input_store;
     std::vector<std::string> output_store;
     std::vector<std::string> wire_store;
-    std::vector<std::string> inv_store;
-    std::vector<std::string> nor_store;
+    std::vector<std::vector<std::string>> gates_store;
 
     for (std::string line; std::getline(ifs, line, '\n');)
     {
@@ -135,7 +137,6 @@ void write_verilog(std::ifstream &ifs, std::ofstream &ofs)
         }
  
         // inv1 g0000(.a(\a[0] ), .O(new_n386_));
-        // imp2 g0001(.a(\b[0] ), .b(new_n386_), .O(new_n387_));
         
         // inv1 g01(.a(pb), .O(new_n8_));
         // nor2 g02(.a(new_n8_), .b(new_n7_), .O(new_n9_));
@@ -143,21 +144,64 @@ void write_verilog(std::ifstream &ifs, std::ofstream &ofs)
         {   
             // inv1 g0000 .a \a[0] .O new_n386_
             std::vector<std::string> str_info = m_split(line, " (),;");
-            for(int i = 1;i < str_info.size(); i++)
+            std::vector<std::string> tempt{};
+            for(int i = 0; i < str_info.size(); i++)
             {
-                inv_store.push_back(str_info[i]);
+                tempt.push_back(str_info[i]);
             }
-            // ofs << "  assign " << str_info[5] << " = " << "~" << str_info[3] << ";" << std::endl;
+            gates_store.push_back(tempt);
+            // std::cout << "  assign " << str_info[5] << " = " << "~" << str_info[3] << ";" << std::endl;
         }
         if(line.find(flag_node2) != std::string::npos)
         {
-            // imp2 g0001 .a \b[0] .b new_n386_ .O new_n387_
+            // nor2 g02 .a new_n8_ .b new_n7_ .O new_n9_
             std::vector<std::string> str_info = m_split(line, " (),;");
-            for(int i =1 ; i < str_info.size(); i++)
+            std::vector<std::string> tempt{};
+            for(int i = 0; i < str_info.size(); i++)
             {
-               nor_store.push_back(str_info[i]);
+               tempt.push_back(str_info[i]);
             }
-            // ofs << "  assign " << str_info[7] << " = " << "~" << str_info[3] << " | " << str_info[5] << ";" << std::endl;
+            gates_store.push_back(tempt);
+            // std::cout << "  assign " << str_info[7] << " = " << "~(" << str_info[3] << " | " << str_info[5] << ");" << std::endl;
+        }
+        if(line.find(flag_node3) != std::string::npos)
+        {
+            // zero g5(.O(Out1));
+            std::vector<std::string> str_info = m_split(line, " ();");
+            std::vector<std::string> tempt{};
+            // zero g5 .O Out1
+            for(int i = 0; i < str_info.size(); i++)
+            {
+                tempt.push_back(str_info[i]);
+            }
+            gates_store.push_back(tempt);
+            // std::cout << "  assign " << str_info[3] << " = 1'b0;" << std::endl;
+        }
+        if(line.find(flag_node4) != std::string::npos)
+        {
+            // buf  g6(.a(c), .O(Out2));
+            std::vector<std::string> str_info = m_split(line, " (),;");
+            std::vector<std::string> tempt{};
+            // buf  g6 .a c .O Out2
+            for(int i = 0; i < str_info.size(); i++)
+            {
+               tempt.push_back(str_info[i]);
+            }
+            gates_store.push_back(tempt);
+            // std::cout << "  assign " << str_info[5] << " = " << str_info[3] << ";" << std::endl;
+        }
+        if(line.find(flag_node5) != std::string::npos)
+        {
+            // one g7(.O(Out2));
+            std::vector<std::string> str_info = m_split(line, " ();");
+            std::vector<std::string> tempt{};
+            // one g7 .O Out2
+            for(int i = 0; i < str_info.size(); i++)
+            {
+                tempt.push_back(str_info[i]);
+            }
+            gates_store.push_back(tempt);
+            // std::cout << "  assign " << str_info[3] << " = 1'b1;" << std::endl;
         }
     }
     std::string top = "module top(";
@@ -174,35 +218,49 @@ void write_verilog(std::ifstream &ifs, std::ofstream &ofs)
     }
     top += ");";
     ofs << top << std::endl;
-     for(int i=0; i<input_store.size(); i++)
-      {
-        ofs << "  input " << input_store[i] << ";" << std::endl;
-      }
-     for(int i = 0; i < output_store.size(); i++)
-      {
-        ofs << "  output " << output_store[i] << ";" << std::endl;
-      }
-     for(int i = 0;i < wire_store.size(); i++)
-      {
-         ofs << "  wire " << wire_store[i] << ";" << std::endl;
-      }
-      for(int i = 0; i < inv_store.size(); i++)
-      {
-        i = i + 4;
-        int t = i - 2;
-        // g0000 .a \a[0] .O new_n386_
-         ofs << "  assign " << inv_store[i] << " = " << "~" << inv_store[t] << ";" <<std::endl;
-        
-      }
-      for(int i =0; i < nor_store.size(); i++)
-      { 
-         i = i + 6;
-        int t = i - 4;
-        int m = i - 2;
-        // g0001 .a \b[0] .b new_n386_ .O new_n387_
-         ofs << "  assign " << nor_store[i] << " = " << "~(" << nor_store[t] << " | " << nor_store[m] << ");" <<std::endl;
-      }
+    for(int i=0; i<input_store.size(); i++)
+    {
+    ofs << "  input " << input_store[i] << ";" << std::endl;
+    }
+    for(int i = 0; i < output_store.size(); i++)
+    {
+    ofs << "  output " << output_store[i] << ";" << std::endl;
+    }
+    for(int i = 0;i < wire_store.size(); i++)
+    {
+        ofs << "  wire " << wire_store[i] << ";" << std::endl;
+    }
+    for(int i = 0; i < gates_store.size(); i++)
+    {
+        //std::cout << gates_store[i][0] << std::endl;
+        if(gates_store[i][0] == "inv1")
+        {
+            //print_function(gates_store[i]);
+            ofs << "  assign " << gates_store[i][5] << " = " << "~" << gates_store[i][3] << ";" << std::endl;
+        }
+        else if(gates_store[i][0] == "nor2")
+        {
+            //print_function(gates_store[i]);
+            ofs << "  assign " << gates_store[i][7] << " = " << "~(" << gates_store[i][3] << " | " << gates_store[i][5] << ");" << std::endl;
+        }
+        else if(gates_store[i][0] == "zero")
+        {
+            //print_function(gates_store[i]);
+            ofs << "  assign " << gates_store[i][3] << " = 1'b0;" << std::endl;
+        }
+        else if(gates_store[i][0] == "buf")
+        {
+            //print_function(gates_store[i]);
+            ofs << "  assign " << gates_store[i][5] << " = " << gates_store[i][3] << ";" << std::endl;
+        }
+        else if(gates_store[i][0] == "one")
+        {
+            //print_function(gates_store[i]);
+            ofs << "  assign " << gates_store[i][3] << " = 1'b1;" << std::endl;
+        }
+    }
     std::string bottom = "endmodule";
     ofs << bottom << std::endl;
+    ofs.close();
 
 }
